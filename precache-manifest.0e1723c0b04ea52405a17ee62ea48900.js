db210015ae735c2b7b051a29c4323fefbeb7b7f4
lucachaco <luiscarlosch@gmail.com>
2020-02-23 14:18:39 -0600

    Updates

---
+++ precache-manifest.0e1723c0b04ea52405a17ee62ea48900.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e20e80900e5fd84a719faa3f15865d",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "fc7afbc8872a296ac2af",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "651907a7e02234f7cfcc",
+    "url": "/react-gh-pages/static/js/2.33af256b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-gh-pages/static/js/2.33af256b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7afbc8872a296ac2af",
+    "url": "/react-gh-pages/static/js/main.46514eee.chunk.js"
+  },
+  {
+    "revision": "ecfd1d276f9241e07c49",
+    "url": "/react-gh-pages/static/js/runtime-main.d24436ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
